8924c5cc4e7503083b8232896796043016a0ea5b
kristina-rad <135635047+kristina-rad@users.noreply.github.com>
2024-05-03 14:51:50 +0300

    Updated resource archive page

--- js/resources-new.js
+++ js/resources-new.js
@@ -1,185 +1,35 @@
-"use strict";
-const checkboxes = document.querySelectorAll('.filter-checkbox');
-const currentUrl = window.location.href;
+Promise.all([
+    import('https://cdn.skypack.dev/@splidejs/splide@2.4.21')
+]).then(([SplideModule]) => {
+    const Splide = SplideModule.default;
+
+    new Splide('#example-grid', {
+        type: 'loop',
+        height: '20rem',
+        perPage: 1,
+        perMove: 1,
+    }).mount();
+}).catch(error => console.error('Error loading Splide:', error));
 
 
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', () => {
-        const input = checkbox.querySelector('input[data-name]');
-        const dataName = input.getAttribute('data-name');
-        const curUrl = new OriginalURL(window.location.href);
-        const resetButton = document.querySelector('.reset-button');
-        //curUrl.searchParams.set('name', dataName);
-        // history.pushState(null, null, curUrl.toString());
-        if (input.checked) {
-            input.disabled = true;
-            checkboxes.forEach(otherCheckbox => {
-                if (otherCheckbox === checkbox) {
-                    otherCheckbox.querySelector('input').disabled = true;
-                }
-                if (otherCheckbox !== checkbox) {
-                    otherCheckbox.querySelector('input').disabled = false;
-                    otherCheckbox.querySelector('input').checked = false;
-                }
-            });
-            const name = checkbox.querySelector('input').getAttribute('name');
+document.addEventListener("DOMContentLoaded", function(){
+    const filterSearchTypeElements = document.querySelectorAll('.filter-search-wrap');
+
+    filterSearchTypeElements.forEach((filterSearchTypeElement, index) => {
+        const titleElement = filterSearchTypeElement.querySelector('.filter-search__header');
+        const contentElement = filterSearchTypeElement.querySelector('.filter-search__content');
+
+        // Remove 'active' class and hide '.filter-search__content' from all except the first
+        if (index !== 0) {
+            titleElement.classList.remove('active');
+            contentElement.classList.add('hidden');
         }
-    });
-});
 
-const filterSearchWrapElements = document.querySelectorAll('.filter-search-wrap');
-const filterSearchTypeElements = document.querySelectorAll('.filter-search-type');
-function isMobile() {
-    return window.innerWidth < 767;
-}
+        titleElement.addEventListener('click', () => {
+            console.log('click');
 
-filterSearchWrapElements.forEach((filterSearchWrapElement) => {
-    const titleElement = filterSearchWrapElement.querySelector('.filter-search__title');
-    const mobileList = filterSearchWrapElement.querySelector('.mobile-list');
-
-    titleElement.addEventListener('click', () => {
-        if (isMobile()) {
-            filterSearchWrapElements.forEach((element) => {
-                const list = element.querySelector('.mobile-list');
-                if (list !== mobileList) {
-                    list.style.display = 'none';
-                }
-            });
-            if (mobileList.style.display === 'block') {
-                mobileList.style.display = 'none';
-            } else {
-                mobileList.style.display = 'block';
-            }
-        }
-    });
-});
-
-filterSearchTypeElements.forEach((filterSearchTypeElement) => {
-    const titleElement = filterSearchTypeElement.querySelector('.filter-search__title');
-    const mobileList = filterSearchTypeElement.querySelector('.mobile-list');
-    titleElement.addEventListener('click', () => {
-        if (isMobile()) {
-            filterSearchTypeElements.forEach((element) => {
-                const list = element.querySelector('.mobile-list');
-                if (list !== mobileList) {
-                    list.style.display = 'none';
-                }
-            });
-            if (mobileList.style.display === 'block') {
-                mobileList.style.display = 'none';
-            } else {
-                mobileList.style.display = 'block';
-            }
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    if (window.innerWidth < 991) {
-        simulateButtonClick();
-    }
-    window.addEventListener('resize', function() {
-        if (window.innerWidth < 991) {
-            simulateButtonClick();
-        }
-    });
-    var openButton = document.getElementById('openButton');
-    var openButton1 = document.getElementById('openButton1');
-    var overlay = document.getElementById('overlay');
-    var popup = document.getElementById('popup');
-    var closeButton = document.getElementById('closeButton');
-    openButton.addEventListener('click', function() {
-        overlay.style.display = 'flex';
-        popup.style.display = 'block';
-    });
-    openButton1.addEventListener('click', function() {
-        overlay.style.display = 'flex';
-        popup.style.display = 'block';
-    });
-    closeButton.addEventListener('click', function() {
-        overlay.style.display = 'none';
-        popup.style.display = 'none';
-    });
-});
-
-function simulateButtonClick() {
-    var buttons = document.querySelectorAll('.resources-more');
-    buttons.forEach(function(button) {
-        button.click();
-    });
-}
-dynamicSlider("role-slider", 1, 1, 2, 1, "", true);
-document.addEventListener("DOMContentLoaded", function() {
-    (function() {
-        const checkboxes = document.querySelectorAll('.filter-item-checkbox');
-        // console.log(checkboxes)
-
-        const updateFilterList = (label, isChecked) => {
-            const filterWrap = document.querySelector('.filter-wrap');
-            let filterList = filterWrap.querySelector('.filter-list');
-
-            if (!filterList) {
-                filterList = document.createElement('div');
-                filterList.classList.add('filter-list', 'w-dyn-items');
-                filterList.setAttribute('role', 'list');
-                filterWrap.appendChild(filterList);
-            }
-            if (isChecked) {
-                const listItem = document.createElement('div');
-                listItem.classList.add('filter-item', 'w-dyn-item');
-                listItem.setAttribute('role', 'listitem');
-                listItem.innerHTML = `<a href="#" class="cat-label w-inline-block"><div class="cat-label__text">${label}</div></a>`;
-
-                listItem.addEventListener('click', (e) => {
-                    try {
-                        const correspondingCheckbox = Array.from(checkboxes).find(checkbox =>
-                            checkbox.closest('.filter-item-row').querySelector('.filtet-item-label')?.textContent === label
-                        );
-                        if (correspondingCheckbox) {
-                            triggerClickEvent(correspondingCheckbox);
-                            correspondingCheckbox.checked = false;
-                        }
-                    } finally {
-                        filterList.removeChild(e.currentTarget);
-                    }
-                });
-                filterList.appendChild(listItem);
-            } else {
-                const existingLabel = Array.from(filterList.children).find(child => child.textContent.trim() === label);
-                if (existingLabel) {
-                    filterList.removeChild(existingLabel);
-                }
-            }
-        };
-        checkboxes.forEach(checkbox => {
-            checkbox.addEventListener('change', (e) => {
-                const label = e.target.closest('.filter-item-row').querySelector('.filtet-item-label').textContent;
-                updateFilterList(label, e.target.checked);
-            });
-        });
-        function triggerClickEvent(checkbox) {
-            const event = new Event('click', {
-                'bubbles': true,
-                'cancelable': true
-            });
-            checkbox.dispatchEvent(event);
-        }
-        document.querySelector('.reset-button').addEventListener('click', (e) => document.querySelector('.filter-list').innerHTML = '');
-    }());
-});
-
-document.querySelectorAll('.resources-nav [href^="/resources/"').forEach((a) => {
-    a.href = `${a.href}#latest`;
-    a.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetElement = document.querySelector(this.getAttribute('href'));
-        const offset = 70; // Высота вашей шапки
-        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - offset;
-
-        window.scrollTo({
-            top: targetPosition,
-            behavior: 'smooth'
+            titleElement.classList.toggle('active');
+            contentElement.classList.toggle('hidden');
         });
     });
 });
-
