d284e2f5aea5ee44376be228018f2ddb48f49d1b
kristina-rad <135635047+kristina-rad@users.noreply.github.com>
2024-07-03 14:33:46 +0300

    Updated demo resources

---
+++ js/demo-resource.js
@@ -0,0 +1,239 @@
+document.addEventListener('DOMContentLoaded', () => {
+        let userInteracted = false;
+
+        const lazyLoadVideo = () => {
+            if (!userInteracted) {
+                userInteracted = true;
+                document.querySelectorAll('.accordion-video-img').forEach(elem => {
+                    elem.style.display = 'none';
+                });
+
+                document.removeEventListener('mousemove', lazyLoadVideo);
+                document.removeEventListener('scroll', lazyLoadVideo);
+                document.removeEventListener('touchstart', lazyLoadVideo);
+                document.removeEventListener('click', lazyLoadVideo);
+            }
+        };
+
+        /* Start Hide Pop-up */
+        function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+            let results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
+
+        function handlePopupHide() {
+            let paramValue = getParameterByName('popup-hide');
+            if (paramValue !== null) {
+                let popup = document.querySelector('.page-pop-up');
+                let body = document.querySelector('body');
+                if (popup) {
+                    popup.style.display = 'none';
+                }
+                if (body) {
+                    body.classList.remove('overflow-hidden');
+                }
+            }
+
+            document.addEventListener('mousemove', lazyLoadVideo);
+            document.addEventListener('scroll', lazyLoadVideo);
+            document.addEventListener('touchstart', lazyLoadVideo);
+            document.addEventListener('click', lazyLoadVideo);
+        }
+
+        handlePopupHide();
+        /* END Hide Pop-up */
+
+        /* Start Pardot Form */
+        function initializeForm() {
+            const form = document.getElementById("pardot-form");
+
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                console.log('function submit')
+
+                const iframe = document.createElement('iframe');
+                iframe.name = 'hidden_iframe';
+                iframe.style.display = 'none';
+                document.body.appendChild(iframe);
+                console.log('iframe ', iframe);
+
+
+                form.action = 'https://go.copado.com/l/372431/2024-06-26/6tpcz1';
+                form.target = 'hidden_iframe';
+
+                const formData = new FormData(form);
+                console.log("Form data:");
+                for (let pair of formData.entries()) {
+                    console.log(pair[0] + ': ' + pair[1]);
+                }
+
+
+                form.submit();
+                console.log('form.submit here')
+
+                iframe.onload = () => {
+                    let currentUrl = window.location.href;
+                    if (currentUrl.indexOf('?') > -1) {
+                        currentUrl += '&popup-hide=';
+                    } else {
+                        currentUrl += '?popup-hide=';
+                    }
+                    window.location.href = currentUrl;
+                };
+            });
+        }
+
+        initializeForm();
+
+        /* END Pardot Form */
+
+        setupAccordion(['.accordion1', '.accordion2', '.accordion3']);
+        initializeFilters();
+
+        /* Start Accordion */
+        function handleAccordion() {
+            const accordionList = document.querySelectorAll('.accordion-list-wrap .accordion-list-mobile');
+
+            function deactivateAll(except = null) {
+                accordionList.forEach(elem => {
+                    if (elem !== except) {
+                        let parent = elem.parentNode;
+                        if (parent && parent.classList.contains('accordion-list-wrap')) {
+                            parent.classList.remove('active');
+                        }
+                    }
+                });
+            }
+
+            accordionList.forEach(elem => {
+                elem.addEventListener('click', () => {
+                    let parent = elem.parentNode;
+                    if (parent && parent.classList.contains('accordion-list-wrap')) {
+                        deactivateAll(elem);
+                        parent.classList.toggle('active');
+                    }
+                });
+            });
+
+            window.addEventListener('resize', () => deactivateAll());
+        }
+
+        handleAccordion();
+        /* END Accordion */
+    }
+)
+;
+
+function setupAccordion(accordionClasses) {
+    accordionClasses.forEach(accordionClass => {
+        const parentElement = document.querySelector(accordionClass);
+        if (!parentElement) return;
+
+        parentElement.addEventListener('click', event => {
+            if (event.target.classList.contains('accordion-list__title')) {
+                const button = event.target;
+                const index = Array.from(parentElement.querySelectorAll('.accordion-list-list .accordion-list__title')).indexOf(button);
+                const video = parentElement.querySelectorAll('.accordion-video-wrap .accordion-video-item')[index];
+
+                resetActiveItems(parentElement);
+                toggleActive(button, video);
+            }
+        });
+    });
+}
+
+function resetActiveItems(parentElement) {
+    parentElement.querySelectorAll('.active').forEach(el => el.classList.remove('active'));
+}
+
+function toggleActive(button, video) {
+    button.classList.add('active');
+    video.classList.add('active');
+}
+
+function initializeFilters() {
+    const filterSearchTypeElements = document.querySelectorAll('.filters-wrap');
+    filterSearchTypeElements.forEach((filterElement, index) => {
+        const titleElement = filterElement.querySelector('.filter-search__header');
+        const contentElement = filterElement.querySelector('.filter-search__content');
+
+        if (index !== 0) {
+            contentElement.classList.add('hidden');
+        }
+
+        titleElement.addEventListener('click', () => {
+            titleElement.classList.toggle('active');
+            contentElement.classList.toggle('hidden');
+        });
+    });
+
+    (function () {
+        const checkboxes = document.querySelectorAll('.filter-item-checkbox');
+
+        const updateFilterList = (label, isChecked) => {
+            const filterWrap = document.querySelector('.filter-wrap');
+            let filterList = filterWrap.querySelector('.filter-list');
+
+            if (!filterList) {
+                filterList = document.createElement('div');
+                filterList.classList.add('filter-list', 'w-dyn-items');
+                filterList.setAttribute('role', 'list');
+                filterWrap.appendChild(filterList);
+            }
+            if (isChecked) {
+                const listItem = document.createElement('div');
+                listItem.classList.add('filter-item', 'w-dyn-item');
+                listItem.setAttribute('role', 'listitem');
+                listItem.innerHTML = `<a href="#" class="cat-label w-inline-block"><div class="cat-label__text">${label}</div></a>`;
+
+                listItem.addEventListener('click', (e) => {
+                    try {
+                        const correspondingCheckbox = Array.from(checkboxes).find(checkbox =>
+                            checkbox.closest('.filter-item-row').querySelector('.filtet-item-label')?.textContent === label
+                        );
+                        if (correspondingCheckbox) {
+                            triggerClickEvent(correspondingCheckbox);
+                            correspondingCheckbox.checked = false;
+                        }
+                    } finally {
+                        filterList.removeChild(e.currentTarget);
+                    }
+                });
+                filterList.appendChild(listItem);
+            } else {
+                const existingLabel = Array.from(filterList.children).find(child => child.textContent.trim() === label);
+                if (existingLabel) {
+                    filterList.removeChild(existingLabel);
+                }
+            }
+        };
+
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', (e) => {
+                const label = e.target.closest('.filter-item-row').querySelector('.filtet-item-label').textContent;
+                updateFilterList(label, e.target.checked);
+            });
+        });
+
+        function triggerClickEvent(checkbox) {
+            const event = new Event('click', {
+                'bubbles': true,
+                'cancelable': true
+            });
+            checkbox.dispatchEvent(event);
+        }
+
+        document.querySelector('.reset-button').addEventListener('click', () => {
+            const filterList = document.querySelector('.filter-list');
+            if (filterList) {
+                filterList.innerHTML = '';
+            }
+        });
+    }());
+}
